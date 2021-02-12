53418b04b90fdbe73cd7020b9c9af7726d749a61
gtretow <guilherme.tretow@gmail.com>
2021-02-12 00:11:32 -0300

    bug fixes

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -3,7 +3,6 @@ import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import Post from "./Post/Post";
-
 import useStyles from "./styles";
 
 const Posts = ({ setCurrentId }) => {
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,21 +1,19 @@
-//reducer é uma função
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
 
 const reducer = (posts = [], action) => {
   switch (action.type) {
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
 
-    case "UPDATE":
-    case "LIKE":
+    case UPDATE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
     //action.payload é o arquivo atualizado
 
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
 
     default:
