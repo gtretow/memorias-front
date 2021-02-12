488c436542bda3580f6a78b868597044c05909f2
gtretow <guilherme.tretow@gmail.com>
2021-02-11 22:42:54 -0300

    like posts, edit posts, delete posts

--- src/Components/Posts/Post/Post.js
+++ src/Components/Posts/Post/Post.js
@@ -11,11 +11,15 @@ import moment from "moment";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { useDispatch } from "react-redux";
 
 import useStyles from "./styles";
+import { deletePost, likePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
   return (
     <Card className={classes.card}>
       <CardMedia
@@ -54,12 +58,24 @@ const Post = ({ post, setCurrentId }) => {
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => {
+            dispatch(likePost(post._id));
+          }}
+        >
           <ThumbUpAltIcon fontSize="small" />
           Like
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => {
+            dispatch(deletePost(post._id));
+          }}
+        >
           <DeleteIcon fontSize="small" />
           Delete
         </Button>
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -2,7 +2,11 @@
 
 const reducer = (posts = [], action) => {
   switch (action.type) {
+    case "DELETE":
+      return posts.filter((post) => post._id !== action.payload);
+
     case "UPDATE":
+    case "LIKE":
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
